98b70dd43cb1d0bc61993354c3b31a07c361f97a
Maverick-333 <abhi.bhatnagar61@yahoo.com>
2025-02-10 16:58:48 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,8 @@
-  #!/bin/bash
-  set -e
+#!/bin/bash
+set -e
 
-  # Pull the latest Docker image from Docker Hub
-  docker pull trainwithshubham/tws-aws-cicd-demo:latest
+# Pull the latest Docker image from your Docker Hub repository
+docker pull maverick334/test-node-app:latest
 
-  # Run the Docker image as a container
-  docker run -d -p 8000:8000 trainwithshubham/tws-aws-cicd-demo:latest
+# Run the Docker image as a container
+docker run -d -p 8000:8000 maverick334/test-node-app:latest
