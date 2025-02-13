e21c21b479e77cfaaf078acfc50b017d94bf3a00
Maverick-333 <abhi.bhatnagar61@yahoo.com>
2025-02-13 16:01:35 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+docker login -u "maverick334" -p "$(aws ssm get-parameter --name "/CodeBuild/myapp/docker-credentials/password" --with-decryption --query Parameter.Value --output text)"
+
+
 # Pull the latest Docker image from your Docker Hub repository
 docker pull maverick334/test-node-app:latest
 
